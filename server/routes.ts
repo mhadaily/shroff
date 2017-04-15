4bae408fea93bff5e11f675a171cb58928b1c30f
Majid <mhadaily@gmail.com>
2017-04-15 19:50:23 +0800

    use chainable express route

--- server/routes.ts
+++ server/routes.ts
@@ -3,12 +3,16 @@ import ExchangeCtrl from './controllers/exchange';
 export default function setRoutes(app) {
 
   const exchange = new ExchangeCtrl();
+
   // API
-  app.route('/api/exchanges').get(exchange.getAll);
-  app.route('/api/exchanges/count').get(exchange.count);
-  app.route('/api/exchanges').post(exchange.insert);
-  app.route('/api/exchanges/:id').get(exchange.get);
-  app.route('/api/exchanges/:id').put(exchange.update);
-  app.route('/api/exchanges/:id').delete(exchange.delete);
+  app.route('/api/exchanges')
+     .get(exchange.getAll)
+     .post(exchange.insert);
+  app.route('/api/exchanges/count')
+     .get(exchange.count);
+  app.route('/api/exchanges/:id')
+     .get(exchange.get)
+     .put(exchange.update)
+     .delete(exchange.delete);
 
 }
