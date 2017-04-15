aead424f5f76707984e43e0b35460692cc5bcd92
Majid <mhadaily@gmail.com>
2017-04-15 18:21:42 +0800

    add getObject Key pipe test

--- client/app/pipes/get-object-key.pipe.spec.ts
+++ client/app/pipes/get-object-key.pipe.spec.ts
@@ -1,8 +1,15 @@
 import { GetObjectKeyPipe } from './get-object-key.pipe';
+const pipe = new GetObjectKeyPipe();
+const smapleObject = { usd: { sell: 123, buy: 321 } };
 
 describe('GetObjectKeyPipe', () => {
+
   it('create an instance', () => {
-    const pipe = new GetObjectKeyPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('transforms one property to single key', () => {
+    expect(pipe.transform(smapleObject)).toBe('usd');
+  });
+
 });
