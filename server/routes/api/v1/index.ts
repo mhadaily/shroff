f9d3674c6b065a0d66e0c8e9d185937d204dfde3
Majid <mhadaily@gmail.com>
2017-04-16 01:13:18 +0800

    add currency Model and Api route

--- server/routes/api/v1/index.ts
+++ server/routes/api/v1/index.ts
@@ -1,6 +1,7 @@
 import exchange from './admin/exchangeComponent/exchange';
+import currency from './admin/exchangeComponent/exchange';
 
 export default function setApiRoutes(app) {
   app.use('/api/exchanges', exchange);
-
+  app.use('/api/currencies', currency);
 };
