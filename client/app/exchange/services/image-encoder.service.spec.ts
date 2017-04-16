209a789592295ca9575bb80db0e82db3b72ceb9b
Majid <mhadaily@gmail.com>
2017-04-16 23:02:03 +0800

    base64Image encoder service

---
+++ client/app/exchange/services/image-encoder.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { ImageEncoderService } from './image-encoder.service';
+
+describe('ImageEncoderService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ImageEncoderService]
+    });
+  });
+
+  it('should ...', inject([ImageEncoderService], (service: ImageEncoderService) => {
+    expect(service).toBeTruthy();
+  }));
+});
