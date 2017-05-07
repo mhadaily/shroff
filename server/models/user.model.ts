f429e0059e95dbcdfe1f74b54a86b4d73a0d8bd5
Majid <mhadaily@gmail.com>
2017-05-07 15:57:32 +0200

    fix firstName typo

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema(
   {
     emailUsername: { type: String, required: true, trim: true, unique: true },
-    fistName: { type: String, required: true, trim: true },
+    firstName: { type: String, required: true, trim: true },
     lastName: { type: String, required: true, trim: true },
     roleLevel: { type: Number, required: false, trim: true, default: 100 },
     password: { type: String, required: true, trim: true }
